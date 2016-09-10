6225892e2005a7aea1700c1867132ce776958a1d
Your Name <you@example.com>
2016-09-10 22:44:56 +0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 
@@ -10,8 +11,40 @@ def post_list(request):
 
 def post_detail(request, pk):
 
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk) # якщо поста нема видае страницю з вказівкою що її нема, тобто 404
     
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    
+    if request.method == "POST":
+        form = PostForm(request.POST) # якщо пост вже написаний
+        if form.is_valid(): # перевірка всі дані попали в пост перед тим як їого зберегти
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk) # перекидає після збереження посту на цю сторінку з відображеням тількі що написаному посту
+    else:
+        form = PostForm() # якщо пост тільки створюєтся
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+
+    post = get_object_or_404(Post, pk=pk) 
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 # Create your views here.
